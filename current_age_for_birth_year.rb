9b31a753a4f4c536db33a8eb06eef17809c73ce7
Amari Ellington <amari.l.ellington@gmail.com>
2019-02-12 18:30:20 +0000

    Automatically backed up by Learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,5 @@
+def current_age_for_birth_year(birth_year) do 
+  it "returns the age of a person based on the year of birth" do
+    age_of_person = current_age_for_birth_year(birth_year)
+  end
+  
